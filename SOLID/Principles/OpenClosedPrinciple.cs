3f65e4da0a085a8774d4559b8aa0f4e22a9b4232
Garrett McTear <garrett.mctear@gmail.com>
2017-08-31 16:47:04 -0600

    OCP

--- SOLID/Principles/OpenClosedPrinciple.cs
+++ SOLID/Principles/OpenClosedPrinciple.cs
@@ -1,6 +1,84 @@
-﻿namespace SOLID.Principles
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace SOLID.Principles
 {
-    public class OpenClosedPrinciple
+    public class InterfaceSegregationPrinciple
     {
+        public class Rectangle
+        {
+            public double Length { get; set; }
+            public double Width { get; set; }
+        }
+
+        public class Calculator1
+        {
+            public double Area(Rectangle[] shapes)
+            {
+                return shapes.Sum(shape => shape.Length * shape.Width);
+            }
+        }
+
+        public class Circle
+        {
+            public double Radius { get; set; }
+        }
+
+        public class Calculator2
+        {
+            public double Area(object[] shapes)
+            {
+                var area = 0.0;
+                foreach(var shape in shapes) {
+                    if (shape is Rectangle)
+                    {
+                        var rectangle = (Rectangle) shape;
+                        area += rectangle.Length * rectangle.Width;
+                    }
+                    else
+                    {
+                        var circle = (Circle)shape;
+                        area += Math.PI * Math.Pow(circle.Radius, 2);
+                    }
+                }
+                return area;
+            }
+        }
+
+        public abstract class Shape
+        {
+            public abstract double Area();
+        }
+
+        public class Rectangle2 : Shape
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+            public override double Area()
+            {
+                return Width * Height;
+            }
+        }
+
+        public class Circle2 : Shape
+        {
+            public double Radius { get; set; }
+            public override double Area()
+            {
+                return Radius * Radius * Math.PI;
+            }
+        }
+
+        public double Calculator3(Shape[] shapes)
+        {
+            double area = 0;
+            foreach (var shape in shapes)
+            {
+                area += shape.Area();
+            }
+
+            return area;
+        }
     }
 }
