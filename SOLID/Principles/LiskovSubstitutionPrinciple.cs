acc225de3fda1a1a218e10bb4dfb78dfcfd557f8
Garrett McTear <garrett@gobanyan.com>
2017-09-05 14:08:45 -0600

    Started LSP

--- SOLID/Principles/LiskovSubstitutionPrinciple.cs
+++ SOLID/Principles/LiskovSubstitutionPrinciple.cs
@@ -2,5 +2,36 @@
 {
     public class LiskovSubstitutionPrinciple
     {
+        public class Rectangle
+        {
+            public virtual int Height { get; set; }
+            public virtual int Width { get; set; }
+        }
+
+        public class Square : Rectangle
+        {
+            private int _height;
+            private int _width;
+
+            public override int Height
+            {
+                get => _height;
+                set
+                {
+                    _height = value;
+                    _width = value;
+                }
+            }
+
+            public override int Width
+            {
+                get => _width;
+                set
+                {
+                    _width = value;
+                    _height = value;
+                }
+            }
+        }
     }
 }
